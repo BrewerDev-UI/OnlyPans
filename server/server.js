2b9b49f3bef96caf58b43fbf10f1e8df5f0d5e88
Devon Verhoff <verhoffdevon@gmail.com>
2023-02-20 10:18:57 -0600

    intial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express();
+const port = 8000
+
+
+const server = app.listen(port, () => console.log(`Ahoy welcome to Port: ${port}`))
+
+const io = require("socket.io")(server, { cors: true })
+
+io.on("connection", (socket) => {
+    console.log(socket.id)
+
+    socket.on("chat", (client_input) => {
+        console.log("Got a message", client_input)
+
+        io.emit("post chat", client_input)
+    })
+})
